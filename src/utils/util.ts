e3c4f581d55ee8ad788e8c2b374e67ee9f23598e
Albert ten Napel <aptennap@gmail.com>
2020-03-13 08:09:53 +0100

    Initial commit

---
+++ src/utils/util.ts
@@ -0,0 +1,24 @@
+export const impossible = (msg: string) => {
+  throw new Error(`impossible: ${msg}`);
+};
+
+export const terr = (msg: string) => {
+  throw new TypeError(msg);
+};
+
+export const serr = (msg: string) => {
+  throw new SyntaxError(msg);
+};
+
+export const loadFile = (fn: string): Promise<string> => {
+  if (typeof window === 'undefined') {
+    return new Promise((resolve, reject) => {
+      require('fs').readFile(fn, 'utf8', (err: Error, data: string) => {
+        if (err) return reject(err);
+        return resolve(data);
+      });
+    });
+  } else {
+    return fetch(fn).then(r => r.text());
+  }
+};
